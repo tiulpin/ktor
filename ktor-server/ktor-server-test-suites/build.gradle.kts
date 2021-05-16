388457a729972a5cb22eacbd20921c4185e92cf7
Oleg Yukhnevich <whyoleg@gmail.com>
2021-05-16 11:52:44 +0300

    KTOR-746 - commonize server-test-suites

--- ktor-server/ktor-server-test-suites/build.gradle.kts
+++ ktor-server/ktor-server-test-suites/build.gradle.kts
@@ -3,16 +3,21 @@ description = ""
 val coroutines_version: String by extra
 
 kotlin.sourceSets {
-    val jvmMain by getting {
+    val commonMain by getting {
         dependencies {
             implementation(project(":ktor-server:ktor-server-test-host"))
+        }
+    }
 
+    val jvmMain by getting {
+        dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$coroutines_version")
         }
     }
+
     val jvmTest by getting {
         dependencies {
-            api(project(":ktor-server:ktor-server-core", configuration= "testOutput"))
+            api(project(":ktor-server:ktor-server-core", configuration = "testOutput"))
         }
     }
 }
